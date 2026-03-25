3effdef6ad28b681939d30a16f4964a3c1b45863
oceluspro <contact@ftnl.fr>
2026-03-25 15:44:37 +0100

    chore: make `BASE_URL` and `BASE_PATH` resolution dynamic using `VITE_BASE_PATH` env variable in Vite config and scripts

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: './',
+  base: process.env.VITE_BASE_PATH || './',
   assetsInclude: ['**/*.jsonc'],
   build: {
     assetsInlineLimit: 10240,

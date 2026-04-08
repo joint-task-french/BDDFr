87055231c2c98b605cadf4e22e86c1c3e900dd24
oceluspro <contact@ftnl.fr>
2026-04-09 01:38:30 +0200

    fix: adjust like button SVG size in BuildLibraryPage for better visibility

--- src/pages/build/BuildLibraryPage.jsx
+++ src/pages/build/BuildLibraryPage.jsx
@@ -832,7 +832,7 @@ function BuildCard({ build, data, onView, onPublish, onDelete, isLocal, apiUrl, 
                               isLiking ? 'opacity-50 cursor-wait' : 'hover:scale-110'
                           } text-shd/80 bg-shd/5 border-shd/20`}
                       >
-                        <svg className="w-3 h-3" fill="currentColor" viewBox="0 0 20 20">
+                        <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
                           <path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" />
                         </svg>
                         {likes}

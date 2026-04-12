a3740a484c1a596f3e6dad87bcd6c75d71518df7
oceluspro <contact@ftnl.fr>
2026-04-12 15:17:59 +0200

    feat: integrate PWA support using `vite-plugin-pwa`
    - Added `vite-plugin-pwa` to Vite configuration for Progressive Web App support with auto-update.
    - Configured PWA manifest with app name, icons, start URL, and theme colors.
    - Updated dependencies to include `vite-plugin-pwa`.

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,40 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      },
+      manifest: {
+        "short_name": "BDDFr",
+        "name": "BDDFr",
+        "icons": [
+          {
+            "src": "favicon_150x150.png",
+            "type": "image/png",
+            "sizes": "150x150",
+            "purpose": "any maskable"
+          },
+          {
+            "src": "favicon_192x192.png",
+            "type": "image/png",
+            "sizes": "192x192"
+          }
+        ],
+        "start_url": process.env.VITE_BASE_PATH || '/BDDFr',
+        "display": "standalone",
+        "theme_color": "#15181d",
+        "background_color": "#0d0f12"
+      }
+    })
+  ],
   base: process.env.VITE_BASE_PATH || '/BDDFr',
   assetsInclude: ['**/*.jsonc'],
   build: {
